0770958dfc6a7d72d19af7f2739a8b486f9bfd11
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-02-21 01:15:05 +0700

    play & footer done

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='flex w-full justify-between px-3 mb-2'>
+        <div className='bg-btn-footer w-[142px] h-[72px] bg-center bg-contain bg-no-repeat
+        relative flex justify-center items-center active:scale-90 duration-100 cursor-pointer'>
+            <Image src="/assets/img/icon-papanperingkat.png" width={68} height={64} className="absolute -top-10" />
+            <div className='flex flex-col text-center mt-4 z-10'>
+                <span className='font-cubano outline-title text-white mt-0.5'>papan</span>
+                <span className='font-cubano outline-title text-white -mt-1 '>peringkat</span>
+            </div>
+        </div>
+        <div className='bg-btn-footer w-[142px] h-[72px] bg-center bg-contain bg-no-repeat
+        relative flex justify-center items-center active:scale-90 duration-100 cursor-pointer'>
+            <Image src="/assets/img/icon-carabermain.png" width={66} height={66} className="absolute -top-11" />
+            <div className='flex flex-col text-center mt-5 z-10'>
+                <span className='font-cubano outline-title text-white mt-0.5'>cara</span>
+                <span className='font-cubano outline-title text-white -mt-1 '>bermain</span>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/app/play.jsx
+++ src/app/play.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Play() {
   return (
-    <div>Play</div>
+    <div className='flex w-full justify-center'>
+        <div className='w-fit'>
+            <div className='bg-playbtn w-44 h-14 bg-center bg-contain bg-no-repeat relative '>
+                <div className='bg-btn-play w-44 h-14 bg-center bg-contain bg-no-repeat absolute -top-2 cursor-pointer z-10 active:scale-90 duration-100 grid place-content-center'>
+                    <h2 className='text-4xl font-cubano outline-title text-white'>play</h2>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
